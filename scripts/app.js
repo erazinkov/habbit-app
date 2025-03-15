3d13d7603b97b9fe1b4a99a4a144252b1f92e001
erazinkov <egorrazinkov@gmail.com>
2025-03-15 12:11:49 +0300

    Added routing with hash

--- scripts/app.js
+++ scripts/app.js
@@ -158,6 +158,7 @@ function rerender(activeHabbitId) {
     if (!activeHabbit) {
         return;
     }
+    document.location.replace(document.location.pathname + '#' + activeHabbitId);
     rerenderMenu(activeHabbit);
     renderHeader(activeHabbit);
     rerenderContent(activeHabbit)
@@ -228,5 +229,12 @@ function addHabbit(event) {
 (() => {
     // saveDataDemo();
     loadData();
-    rerender(habbits[0].id);
+    const hashId = Number(document.location.hash.replace('#', ''));
+    const urlHabbit = habbits.find(habbit => habbit.id == hashId);
+    if (urlHabbit) {
+        rerender(urlHabbit.id);
+    } else {
+        
+        rerender(habbits[0].id);
+    }
 })();
